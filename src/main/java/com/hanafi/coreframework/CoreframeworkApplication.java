e8701ed8b109975e60a395b073374d4fe4d76eb4
Anasse HANAFI <anasse@Anasses-MacBook-Pro.local>
2024-10-24 22:18:56 +0100

    First commit

---
+++ src/main/java/com/hanafi/coreframework/CoreframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.hanafi.coreframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreframeworkApplication.class, args);
+	}
+
+}
