abdc194b37c9aa90424bb8d4b382fced73c817dc
anasse <anasse.hanafi94@gmail.com>
2024-10-27 23:45:27 +0100

    Rename main class name

--- src/main/java/com/hanafi/coreframework/CoreFrameworkApplication.java
+++ src/main/java/com/hanafi/coreframework/CoreFrameworkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreframeworkApplication {
+public class CoreFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreframeworkApplication.class, args);
+		SpringApplication.run(CoreFrameworkApplication.class, args);
 	}
 
 }
