16aed7045ea8bfdff24061dc94b3464d4ebf627b
anasse <anasse.hanafi94@gmail.com>
2024-11-24 11:28:45 +0100

    custom exception for AuthorizationDeniedException

--- src/main/java/com/hanafi/coreframework/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hanafi/coreframework/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.hanafi.coreframework.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -9,18 +10,14 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-//     Handle specific exceptions
-//    @ExceptionHandler(ResourceNotFoundException.class)
-//    public ResponseEntity<ApiError> handleResourceNotFoundException(
-//            ResourceNotFoundException ex, WebRequest request) {
-//        ApiError error = new ApiError(
-//                HttpStatus.NOT_FOUND.value(),
-//                ex.getMessage(),
-//                request.getDescription(false).replace("uri=", ""));
-//        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-//    }
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ApiError> handleAuthorizationDeniedException(Exception ex, WebRequest request) {
+            ApiError error = new ApiError(
+            HttpStatus.FORBIDDEN.value(),
+                ex.getMessage(), request.getDescription(false).replace("uri=", ""));
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-    // Handle generic exceptions
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleGlobalException(Exception ex, WebRequest request) {
         ApiError error = new ApiError(
